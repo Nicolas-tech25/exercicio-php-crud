372cc96ce32723284493ac8d39b8bccbbcc4c772
@Nicolas_Oliveira <125362779+Nicolas-tech25@users.noreply.github.com>
2023-09-03 19:02:31 -0300

    revisão final

--- visualizar.php
+++ visualizar.php
@@ -11,68 +11,64 @@ $lista_alunos = lerAlunos($conexao);
 <title>Lista de alunos - Exercício CRUD com PHP e MySQL</title>
 <link href="css/style.css" rel="stylesheet">
 </head>
-<body>
-<div class="container">
-    <h1>Lista de alunos</h1>
-    <hr>
-    
+    <body>
+        <article class="container">
+            <h1>Lista de alunos</h1>
+            <hr>
+            
 
-   <!-- Aqui você deverá criar o HTML que quiser e o PHP necessários
-para exibir a relação de alunos existentes no banco de dados.
+            <!-- Aqui você deverá criar o HTML que quiser e o PHP necessários
+            para exibir a relação de alunos existentes no banco de dados.
 
-Obs.: não se esqueça de criar também os links dinâmicos para
-as páginas de atualização e exclusão. -->
-  
+            Obs.: não se esqueça de criar também os links dinâmicos para
+            as páginas de atualização e exclusão. -->
+        
 
             <table>
-
                 <thead>
+                    <caption><a href="inserir.php">Inserir novo aluno</a></caption>
 
-                <caption><a href="inserir.php">Inserir novo aluno</a></caption>
-
-                <tr>
-                    <th>Nome</th>
-                    <th>Nota 01</th>
-                    <th>Nota 02</th>
-                    <th>Media</th>
-                    <th>Situação</th>
-                    <th>Edições</th>
-                    <th cosplan="2"></th>
-                </tr>
-
+                    <tr>
+                        <th>Nome</th>
+                        <th>Nota 01</th>
+                        <th>Nota 02</th>
+                        <th>Media</th>
+                        <th>Situação</th>
+                        <th>Edições</th>
+                        <th cosplan="3"></th>
+                    </tr>
                 </thead>
-
                 <tbody>
-                <?php foreach($lista_alunos as $alunos) { 
+                    <?php foreach($lista_alunos as $alunos) { 
                     $media = $alunos["media"]; ?>
 
                     <tr>
                         <td><?=$alunos["nome"]?> </td>
                         <td><?=$alunos["nota_um"]?></td>
                         <td><?=$alunos["nota_dois"]?></td>
-                        <td><?=CalculoMedia($alunos["nota_um"],$alunos["nota_dois"])?></td>
+                        <td>
+                            <?=CalculoMedia($alunos["nota_um"],$alunos["nota_dois"])?>
+                        </td>
                         <td>
                             <?php
 
                                 if ($media >= 7) {
-                                    echo  "<p class='aprovado'>aprovado!</p>";    
+                                    $situacao =  "<p class='aprovado'>aprovado!</p>";    
                                 } else{
-                                    echo  "<p class='reprovado'>reprovado!</p>";  
+                                    $situacao =  "<p class='reprovado'>reprovado!</p>";  
                                 } ?>
+
+                                <?=$situacao?>
                         </td>
                         <td>
-                             <a href="atualizar.php?id=<?=$alunos["id"]?>">Editar</a> | 
+                            <a href="atualizar.php?id=<?=$alunos["id"]?>">Editar</a> | 
                             <a class="excluir" href="excluir.php?id=<?=$alunos["id"]?>">Excluir</a> 
                             <?php } ?>
                         </td>     
-
                     </tr>
                 </tbody>
             </table>
-
-
-    <p><a href="index.php">Voltar ao início</a></p>
-</div>
-
-</body>
+            <p><a href="index.php">Voltar ao início</a></p>
+        </article>
+    </body>
 </html>
